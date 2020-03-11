432987f882aafa091429957e8f22f9656ab47700
rachmatwahid <rachmatwahid@gmail.com>
2020-03-11 17:27:33 +0700

    initial commit

--- learn_function.py
+++ learn_function.py
@@ -26,7 +26,8 @@ biografi(kota="Jakarta", nama="Dian")
 # Membuat function dengan parameter yang nilainya default
 def identitas(nama = "Fulan"):
     print("Hai, nama saya " + nama)
-identitas() # argumen tidak ada
+identitas() # argumen tidak ada, Fulan menjadi argumennya
+identitas("Dian")
 
 # Membuat function yang bisa mengembalikan nilai
 def hitung_pangkat(angka):
@@ -38,11 +39,15 @@ print(hitung_pangkat(2))
 # Cakupan Variabel
 # ================
 
+# 1. Jika variabel berada di luar function, disebut variabel global
+# 2. Jika berada di dalam function, disebut variabel lokal
+# 3. Walaupun identifier-nya sama, tapi nilainya berbeda
+
 hasil = 0 # ini variabel global
 
 def penjumlahan(angka1,angka2):
     hasil = angka1 + angka2
     return hasil # ini variabel lokal
 
-print("Local variable " + str(penjumlahan(1,2)))
-print("Global variable " + str(hasil))
+print("Local variable " + str(penjumlahan(1,2))) # nilainya 3
+print("Global variable " + str(hasil)) # nilainya nol
